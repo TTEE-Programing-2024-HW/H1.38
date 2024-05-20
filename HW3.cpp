158f21a7231d6e56a2bd6b15844f03f427892316
david <u11219038@o365.ttu.edu.tw>
2024-05-20 19:32:05 +0800

    打上個人風格

--- HW3.cpp
+++ HW3.cpp
@@ -1,79 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> // Include delay function sleep()
 
-// 預設密碼
-#define PASSWORD 2024
+#define PASSWORD "2024" // Define password constant
 
-// 函數宣告
-void displayWelcomeScreen();
-int verifyPassword();
-void displayMainMenu();
-
-// 主程式
 int main() {
-    int passwordCorrect = 0;
+    int attempts = 0;
+    char input[5];
 
-    displayWelcomeScreen();
-
-    // 驗證密碼
-    for (int i = 0; i < 3; i++) {
-        if (verifyPassword()) {
-            passwordCorrect = 1;
-            break;
-        } else {
-            printf("密碼錯誤！請再試一次。\n");
-        }
-    }
-
-    if (passwordCorrect) {
-        printf("密碼正確！\n");
-        displayMainMenu();
-    } else {
-        printf("密碼連續錯誤三次，程式結束。\n");
-    }
-
-    return 0;
-}
-
-// 顯示個人風格的畫面
-void displayWelcomeScreen() {
-    printf("************************************\n");
-    printf("*                                  *\n");
-    printf("*             *\n");
-    printf("*                                  *\n");
-    printf("*               *\n");
+    // Print initial interface
+    printf("************************************\n"); 
+    printf("*                                  *\n"); 
     printf("*                                  *\n");
     printf("*                                  *\n");
-    printf("*        請輸入密碼以繼續          *\n");
+    printf("*                 1                *\n"); 
+    printf("*                 2                *\n");
+    printf("*                 3                *\n");
+    printf("*                 4                *\n");
+    printf("*                 5                *\n");
+    printf("*                 6                *\n");
+    printf("*                 7                *\n");
+    printf("*                 8                *\n");
+    printf("*                 9                *\n");
+    printf("*                 10               *\n");
+    printf("*                 11               *\n");
+    printf("*                 12               *\n");
+    printf("*                 13               *\n");
+    printf("*                 14               *\n");
+    printf("*                 15               *\n");
     printf("*                                  *\n");
-    printf("************************************\n");
-}
+    printf("*                                  *\n");
+    printf("*                                  *\n");
+    printf("************************************\n"); // Print bottom border
 
-// 驗證密碼
-int verifyPassword() {
-    int inputPassword;
-
-    printf("請輸入4位數密碼：");
-    scanf("%d", &inputPassword);
-
-    if (inputPassword == PASSWORD) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-// 顯示主選單
-void displayMainMenu() {
-    system("clear"); // 清除螢幕 (在 Windows 上使用 system("cls"))
-    printf("----------[BookingSystem]----------\n");
-    printf("|  a. Available seats              |\n");
-    printf("|  b. Arrange for you              |\n");
-    printf("|  c. Choose by yourself           |\n");
-    printf("|  d. Exit                         |\n");
-    printf("------------------------------------\n");
-
-    // 選單功能可在此處繼續實作
-}
-
+  
